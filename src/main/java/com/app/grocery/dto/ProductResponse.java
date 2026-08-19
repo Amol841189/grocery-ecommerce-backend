dfd297c7ccc686692df52d167ab6dbeb8a2d6669
Amol841189 <amol841189@gmail.com>
2026-08-19 19:25:36 +0530

    Remove unused imports from ProductResponse

--- src/main/java/com/app/grocery/dto/ProductResponse.java
+++ src/main/java/com/app/grocery/dto/ProductResponse.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
